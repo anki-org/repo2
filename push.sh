81908ecbca5f643e51077f9a23af395da18c9832
ankit85jsin <ankit85jsin@yahoo.co.in>
2020-03-06 17:57:04 +0530

    commit

--- push.sh
+++ push.sh
@@ -3,5 +3,5 @@ git add --all
 git config --local user.email "ankit85jsin@yahoo.co.in"
 git config --local user.name "ankit85jsin"
 git commit -m "commit"
-git remote set-url origin https://430705eefe808ae88ab86a8a85054c83261cceb0:${GITHUB_TOKEN1}@github.com/anki-org/repo2.git
+git remote set-url origin https://f337fbcb0d0145bf9c878392d7fd38e5e3aae27a:${GITHUB_TOKEN1}@github.com/anki-org/repo2.git
 git push -f origin master
